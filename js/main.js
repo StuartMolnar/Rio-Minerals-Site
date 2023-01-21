112eec709a801ea835aef2587d326568ad61c9c3
StuartMolnar <stuartmolnar@hotmail.com>
2023-01-20 23:44:15 -0800

    contact page ui completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const initApp = () => {
+    //mobile menu
     hamburgerBtn = document.getElementById('hamburger-button');
     const mobileMenu = document.getElementById('mobile-menu');
 
@@ -10,6 +11,34 @@ const initApp = () => {
 
     hamburgerBtn.addEventListener('click', toggleMenu);
     mobileMenu.addEventListener('click', toggleMenu)
+
 }
 
+//contact page map
+function initMap() {
+
+    // The location of Rio Minerals office
+    const rio_office = {
+      lat: 49.2852619,
+      lng: -123.1164446
+    };
+  
+    // Create the map, centered at rio_office
+    const map = new google.maps.Map(
+        document.getElementById("map"), {
+  
+      // Set the zoom of the map
+      zoom: 18,
+      center: rio_office,
+    });
+
+    new google.maps.Marker({
+        position: rio_office,
+        map,
+        title: "Rio Minerals Office",
+      });
+    
+  }
+
 document.addEventListener('DOMContentLoaded', initApp);
+
