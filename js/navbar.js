a3fae87f0e503144f15d5e52b8d43113ffda58e9
StuartMolnar <stuartmolnar@hotmail.com>
2023-03-10 17:38:50 -0800

    functioning navbar minimizer

--- js/navbar.js
+++ js/navbar.js
@@ -10,22 +10,69 @@
 //   }
   
 //   window.onscroll = function() {minimizeNav()}
+
+//mobile menu
+const initMobileMenu = () => {
+
+    
+    hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        document.body.classList.toggle('overflow-y-hidden')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu)
+
+}
+
   
-  //mobile menu
-  const initMobileMenu = () => {
-      
-      hamburgerBtn = document.getElementById('hamburger-button');
-      const mobileMenu = document.getElementById('mobile-menu');
+const initNavbarMinimizer = () => {
+    const header = document.getElementById('navbar-full');
+    const scrollHeight = window.innerHeight - 80; // adjust the value 80 to your preference
   
-      const toggleMenu = () => {
-          mobileMenu.classList.toggle('hidden');
-          mobileMenu.classList.toggle('flex');
-          document.body.classList.toggle('overflow-y-hidden')
-      }
+    const navbarMin = document.getElementById('navbar-min');
+    const navbarMax = document.getElementById('navbar-max');
   
-      hamburgerBtn.addEventListener('click', toggleMenu);
-      mobileMenu.addEventListener('click', toggleMenu)
+    navbarMin.addEventListener('click', () => {
+        header.classList.toggle('scrolled');
+        navbarMax.classList.toggle('hidden');
+        navbarMin.classList.toggle('hidden');
+        // Rotate the image by 180 degrees when the navbar-min element is clicked
+        const navbarMinIcon = document.querySelector('#navbar-min .navbar-min-icon');
+        if (navbarMinIcon.style.transform === 'rotate(180deg)') {
+          navbarMinIcon.style.transform = 'rotate(0deg)';
+        } else {
+          navbarMinIcon.style.transform = 'rotate(180deg)';
+        }
+        navbarMin.style.display = 'flex';
+      });
   
-  }
+      window.addEventListener('scroll', () => {
+        if (window.pageYOffset > scrollHeight) {
+          header.classList.add('scrolled');
+          navbarMax.classList.add('hidden');
+          navbarMin.classList.remove('hidden');
+          // Rotate the image back to 0 degrees when the user scrolls
+          const navbarMinIcon = document.querySelector('#navbar-min .navbar-min-icon');
+          navbarMinIcon.style.transform = 'rotate(0deg)';
+          navbarMin.style.display = 'flex';
+        } else {
+          header.classList.remove('scrolled');
+          navbarMax.classList.remove('hidden');
+          navbarMin.classList.add('hidden');
+          navbarMin.style.display = 'none';
+        }
+      });
+  };
+  
+  
+
+const navbarFull = document.getElementById('navbar-full');
+const navbarPlaceholder = document.getElementById('navbar-spacer');
+navbarPlaceholder.style.height = `${navbarFull.offsetHeight}px`;
 
   
